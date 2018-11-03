8478c8a2983557852ce78bd474ee80168b5dfc86
Crystal Yungwirth <crystalyungwirth@gmail.com>
2018-11-03 11:31:56 -0400

    import packages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
+import 'normalize.css';
+import PropTypes from 'prop-types';
 
 export default class App extends Component {
   render() {
     return (
       <>
-      
+
       </>
     );
   }
