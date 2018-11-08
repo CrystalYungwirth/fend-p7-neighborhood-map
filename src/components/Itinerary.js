ae03a81bbffd863815b909b18c5384976cd22a10
Crystal Yungwirth <CrystalYungwirth@gmail.com>
2018-11-08 16:36:05 +0000

    fix: list items require aria role attributes

--- src/components/Itinerary.js
+++ src/components/Itinerary.js
@@ -8,32 +8,32 @@ class Itinerary extends Component {
     query: ""
   };
 
-    /**
-     * @description run updateQuery if update
-     */
-    componentDidUpdate = prevProps => {
-      if (this.props.value !== prevProps.value) {
-        this.updateQuery();
-      }
-    };
+  /**
+   * @description run updateQuery if update
+   */
+  componentDidUpdate = prevProps => {
+    if (this.props.value !== prevProps.value) {
+      this.updateQuery();
+    }
+  };
 
-    /**
-     * @description bind user input to handleChange
-     * @param {string} query
-     */
-     updateQuery = (newQuery) => {
-         this.setState({ query: newQuery });
-         this.props.handleChange(newQuery);
-     }
+  /**
+   * @description bind user input to handleChange
+   * @param {string} query
+   */
+  updateQuery = newQuery => {
+    this.setState({ query: newQuery });
+    this.props.handleChange(newQuery);
+  };
 
-    /**
-     * @description update query value state
-     * @param {string} query
-     */
-    handleChange = (event, query) => {
-      this.setState({ query });
-      this.updateQuery(event.target.value);
-    };
+  /**
+   * @description update query value state
+   * @param {string} query
+   */
+  handleChange = (event, query) => {
+    this.setState({ query });
+    this.updateQuery(event.target.value);
+  };
 
   render() {
     return (
@@ -45,24 +45,30 @@ class Itinerary extends Component {
           onChange={this.handleChange}
           value={this.state.query}
         />
-      <ComponentSlider>
-        <ul className="stops">
-          {this.props.stops &&
-            this.props.stops.map((stop, index) => {
-              return (
-                <li key={stop.name}>
-                  <button
-                    className="info-card slider-content"
-                    key={stop.id}
-                    onClick={event => this.props.handleClick(index)}
+        <ComponentSlider>
+          <ul className="stops">
+            {this.props.stops &&
+              this.props.stops.map((stop, index) => {
+                return (
+                  //listbox role creates an implicit value of aria-orientation vertical, so I declard it back to horizontal.
+                  //https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/listbox_role
+                  <li
+                    key={stop.name}
+                    role="listbox"
+                    aria-orientation="horizontal"
                   >
-                    {stop.name}
-                  </button>
-                </li>
-              );
-            })}
-        </ul>
-      </ComponentSlider>
+                    <button
+                      className="info-card slider-content"
+                      key={stop.id}
+                      onClick={event => this.props.handleClick(index)}
+                    >
+                      {stop.name}
+                    </button>
+                  </li>
+                );
+              })}
+          </ul>
+        </ComponentSlider>
       </>
     );
   }
